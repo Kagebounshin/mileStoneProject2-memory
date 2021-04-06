aa4ba0ec81b52c670a59ccaf6e0a8d281b3102cf
Kagebounshin <kristoffer-heiskanen@hotmail.com>
2021-04-06 13:12:44 +0000

    added two functions reavelGameChoices and an window.onload

---
+++ assets/js/script-home.js
@@ -0,0 +1,33 @@
+
+// hides the container when the page os loaded.
+window.onload = function () {
+    $(".gameChoice-container").hide();
+};
+
+
+// When you click on either of the answers
+// the game choices will reavel,
+// and your answer options will disapear.
+// Your answer will be displayed
+
+document.getElementByClassName("gameChoice-container").addEventListener(click, revealGameChoices);
+
+function revealGameChoices() {
+    
+    
+    var a1 = document.getElementsByClassName("answer-1");
+    var a2 = document.getElementsByClassName("answer-2");
+    var a3 = document.getElementsByClassName("answer-3");
+
+    if (a1) {
+        document.getElementById("myAnswer").innerHTML = 'YOU - "Ooow, my hëad!"' ;
+    } else if (a2) {
+        document.getElementById("myAnswer").innerHTML = 'YOU - "Yes, I\'m a detective, I knew that. (lie)"';
+    } else if (a3) {
+        document.getElementById("myAnswer").innerHTML = 'YOU - "I\'m a SUPERCOP!"';
+    }
+    
+    $(".gameChoice-container").slideDown('slow');
+    $(".menu-answer-box").hide();
+}
+
