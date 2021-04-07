0544d064ec058439e5be5f6e7c780a37c51c03fb
Kagebounshin <kristoffer-heiskanen@hotmail.com>
2021-04-07 08:58:16 +0000

    Added TO DO comments

---
+++ assets/js/game-script.js
@@ -0,0 +1,8 @@
+// TO DO 
+// Make a card holder and a function wich will push the cards on the gameboard and it going to push two of the same card.
+// a Suffle function wich will suffle the cards. 
+// A Flip function to reavel the cards. 
+// A function that will see if the cards are a match. If they are a match display the cards if != match flip the cards back. 
+// A timer and a move count.
+// an alert message with the amount of time it took and the move count maybe a star rating depending on how many moves. 
+
