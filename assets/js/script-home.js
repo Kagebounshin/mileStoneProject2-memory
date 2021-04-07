981ec1e31039a05da4e8d7bf0950906ba7fd713d
Kagebounshin <kristoffer-heiskanen@hotmail.com>
2021-04-07 10:16:15 +0000

    changes to the revealGameChoices function

--- assets/js/script-home.js
+++ assets/js/script-home.js
@@ -16,10 +16,16 @@ function revealGameChoices() {
     
     
     var a1 = document.getElementById("answer-1").innerHTML;
+    var a2 = document.getElementById("answer-2").innerHTML;
+    var a3 = document.getElementById("answer-3").innerHTML;
 
     if (a1) {
         document.getElementById("myAnswer").innerHTML = 'YOU - "Ooow, my hëad!"' ;
-    } 
+    } else if (a2) {
+        document.getElementById("myAnswer").innerHTML = 'YOU - "Yes, I\'m a detective, I knew that. (lie)"';
+    } else if (a3) {
+        document.getElementById("myAnswer").innerHTML = 'YOU - "I\'m a SUPERCOP!"';
+    }
     
     $(".gameChoice-container").slideDown('slow');
     $(".menu-answer-box").hide();
